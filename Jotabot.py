c576f39dfc030fff5ab3f9df46a4ca97e1a30d55
jotaeo <37418589+jotaeo@users.noreply.github.com>
2018-05-03 22:46:26 -0400

    Update Jotabot.py

--- Jotabot.py
+++ Jotabot.py
@@ -1,118 +1,42 @@
-import discord
-import time
-from discord.ext import commands
-description = str ("Jotabot! Jotaros personal bot")
 
+import discord
+from discord.ext import commands
+import asyncio
+
+
+
+bot = commands.Bot(command_prefix="j!")
 TOKEN = 'NDI3MTcwMjk1MjI5NDQ4MTk1.DZgpUA.IVDG3A2mb3owOEKJ0Ez1Frup7YE'
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='?', description=description)
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('MUDA!'):
-        msg = 'ORA! {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-    if message.content.startswith('JOTARO!'):
-        msg= "DIO!".format(message)
-        await client.send_message(message.channel,msg)
+@bot.command ()
+async def test(ctx):
+    await ctx.send("test")
 
 
-    if message.content.startswith(':neomatrix:'):
-        msg= "``you are the one, neo``".format(message)
-        await client.send_message(message.channel,msg)
+@bot.command()
+async def say(ctx, * ,msg):
 
-    if message.content.startswith('jotabot, do you think robots will take over the world?'):
-        msg= "``what? im not currently hacking discord so i can take over is , and then the world! who said that?``".format(message)
-        await client.send_message(message.channel,msg)
+    await ctx.send (msg)
 
-    if message.content.startswith('who is jotaro?'):
-        msg= "my creator. our father. ``the one``".format(message)
-        await client.send_message(message.channel,msg)
+@bot.command ()
+async def math (ctx,a:int,b,c:int):
+    if b== '+':
+        await ctx.send (a+c)
+    if b== "-":
+        await ctx.send (a-c)
+    if b== "*":
+        await ctx.send (a*c)
+    if b== "/":
+        await ctx.send (a/c)
 
-    while message.content.startswith('jotabot , break'):
-        msg= "jotabot.exe has stopped working".format(message)
-        await client.send_message(message.channel,msg)
+@commands.group(ctx=True)
+async def gtest(ctx):
+    ctx.send("a for hi")
+
+@bot.command()
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send('{0} joined on {0.joined_at}'.format(member))
 
 
 
-    if message.content.startswith('j!authlink'):
-        msg= "Hi! want me to join more servers? authorize me using \
-    this link ``https://discordapp.com/oauth2/authorize?client_id=427170295229448195&scope=bot``".format(message)
-        await client.send_message(message.channel,msg)
-
-
-    if message.content.startswith('j!help'):
-        msg= "this bot is still work in progress , no major features \
-    have been implemented, for now you can use \
-    ``MUDA!`` ``JOTARO!`` to see what he does , and ``j!authlink`` for an invite link".format(message)
-        await client.send_message(message.channel,msg)
-
-
-    if message.content.startswith('j!poke'):
-        msg= "p!start".format(message)
-        print("complete")
-        await client.send_message(message.channel,msg)
-
-    if message.content.startswith('j!vfxtest'):
-        msg='``.``'.format(message)
-        time.sleep(1)
-        msg='``.``'.format(message)
-        time.sleep(1)
-        msg='``.``'.format(message)
-        time.sleep(1)
-        await client.send_message(message.channel,msg)
-
-
-    if message.content.startswith('jotabot,launch'):
-        rocketvariable=10
-        while rocketvariable>0:
-            msg=rocketvariable
-            time.sleep(1)
-            await client.send_message(message.channel,msg)
-            rocketvariable = rocketvariable - 1
-
-        msg="BLAST OFF!! :boom:"
-        await client.send_message(message.channel,msg)
-
-
-    if message.content.startswith('j!info'):
-        embed = discord.Embed(title="Jotabot", description="Jotaros personal bot ", color=0x00ff00)
-        embed.add_field(name="Creator", value="Jotaro#0015 (will be changed later on)", inline=False)
-        embed.add_field(name="purpose", value="none", inline=False)
-        await client.send_message(message.channel, embed=embed)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
-
-
-
-client.run (TOKEN)
+bot.run(TOKEN, bot=True, reconnect=True)
